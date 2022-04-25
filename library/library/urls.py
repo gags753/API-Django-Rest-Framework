59f1b511cbbfb01289dcceb82660d0d51bd9758d
Gabriel Augusto <gabriel.augusto@grupoportfolio.com.br>
2022-04-25 16:25:48 -0300

    adicionando jwt

--- library/library/urls.py
+++ library/library/urls.py
@@ -24,11 +24,15 @@ from django.conf.urls.static import static
 
 from django.conf import settings
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 route = routers.DefaultRouter()
 
 route.register(r'books', booksviwesets.BooksViewSet, basename="Books")
 
 urlpatterns = [
     path('admin', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('', include(route.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
